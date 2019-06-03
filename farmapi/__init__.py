fb62b6d0f07942aab37ce1edd9bd0d4c97260255
Timothy Njoroge <njorogetimk@gmail.com>
2019-06-03 21:35:59 +0300

    Add flask-cors extention

--- farmapi/__init__.py
+++ farmapi/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from farmapi.farm.config import ProductionConfig
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(ProductionConfig)
+CORS(app)
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
